194d2d007ce442fc30038b043c2cf7e64f34f79e
justin-wolfe-z <justin.wolfe@zapier.com>
2017-11-17 14:07:40 -0500

    thinking through state structure, adding some more properties

--- src/data/initial_store.js
+++ src/data/initial_store.js
@@ -4,29 +4,38 @@ const initialState = {
 		{
 			id:1,
 			icon:"baby",
+			type:"static",
+			connected:"false",
 			text:"",
 			hookURL:"hookURL"
 		},
 		{
 			id:2,
 			icon:"hat",
+			type:"static",
+			connected:"false",
 			text:"",
 			hookURL:"hookURLLL",
 		},
 		{
 			id:3,
 			icon:"puppy",
+			type:"static",
+			connected:"false",
 			text:"",
 			hookURL:"hookURLLL",
 		},
 		{
 			id:4,
 			icon:"cookie",
+			type:"static",
+			connected:"false",
 			text:"",
 			hookURL:"hookURLLL",
 		}
 	],
 	isFetching: false,
+	isSelecting: false,
 	isLoggedIn: false,
 	screen: "Pushboard"
 }
