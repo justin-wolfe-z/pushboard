f0383919e7889061ec2180f67cbe5e9da6873c00
justin-wolfe-z <justin.wolfe@zapier.com>
2017-12-21 17:26:13 -0500

    finally understanding calling a function from a parent component in a child component

--- src/components/AppButton.js
+++ src/components/AppButton.js
@@ -3,9 +3,19 @@ import {connect} from 'react-redux'
 import {clickButton,loginThunk} from '../actions/index'
 
 class AppButton extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props)
+    this.click = this.click.bind(this);
+  }
+  click() {
+    console.log('child click')
+    this.props.parentClick('click')
+    console.log('after parent click')
+  }
 	render() {
 		return (
-			<div className='AppButton'>
+			<div className='AppButton' onClick={this.click}>
 				{this.props.name}
 			</div>
 		)
@@ -20,9 +30,6 @@ const mapStateToProps = state => {
 //there has to be a better way to do this!
 const mapDispatchToProps = dispatch => {
   return {
-    click: (props) => {
-    	console.log("click triggered")
-    }
   }
 }
 
--- src/screens/Pushboard.js
+++ src/screens/Pushboard.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import StatusBar from '../components/StatusBar'
 import Board from '../components/Board'
-import ButtonBar from '../components/ButtonBar'
+import AppButton from '../components/AppButton'
 import '../components/App.css';
 import {AppButtons} from '../data/shared_constants'
 
 class Pushboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {email:'', key:'', error:''};
+  }
+  parentClick(name){
+    console.log("parentclick")
+    console.log(name)
+  }
   render() {
     return (
       <div className='Pushboard'>
         <StatusBar />
         <Board />
-        <ButtonBar buttons={AppButtons['Pushboard']}/>
+        <AppButton name='click' parentClick={this.parentClick}/>
       </div>
     )
   }
