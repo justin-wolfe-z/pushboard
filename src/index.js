f1c909ab9d43565b00c8d4928c2e84940ffd3767
justin-wolfe-z <justin.wolfe@zapier.com>
2017-12-24 13:34:49 -0500

    added hot reloading following this guide http://joshbroton.com/add-react-hot-reloading-create-react-app/

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { render } from 'react-dom'
 import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { AppContainer } from 'react-hot-loader';
 import logger from 'redux-logger'
 import thunk from 'redux-thunk';
 import App from './components/App'
@@ -12,17 +13,23 @@ const store = createStore(reducer, applyMiddleware(thunk,logger))
 
 render(
   <Provider store={store}>
+  <AppContainer>
     <App />
+  </AppContainer>
   </Provider>,
   document.getElementById('root')
 )
 
 if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    const NextApp = require('./components/App').default
-    ReactDOM.render(
-      <NextApp />,
-      document.getElementById('root')
-    )
-  })
+    module.hot.accept('./components/App', () => {
+        const NextApp = require('./components/App').default; // eslint-disable-line global-require
+        ReactDOM.render(
+        	<Provider store={store}>
+            <AppContainer>
+                <NextApp />
+            </AppContainer>
+          </Provider>,
+            document.getElementById('root')
+        );
+    });
 }
