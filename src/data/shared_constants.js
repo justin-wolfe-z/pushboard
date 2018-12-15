728b3b3d32843af2fbaeb20d4c8dc01f86d16d4d
justin-wolfe-z <justin.wolfe@zapier.com>
2018-12-15 10:35:01 -0500

    updated base URL for api

--- src/data/shared_constants.js
+++ src/data/shared_constants.js
@@ -1,13 +1,13 @@
 export const URLs = {
-	base: 'https://tap.justinwolfe.me',
-	route: {
-		login: ['GET','/user'],
-		signup: ['POST','/user'],
-		reset: ['POST','/reset'],
-		push: ['POST','/push'],
-		save: ['POST','/save'],
-		quit: ['POST','/quit']
-	}
-}
+  base: "https://zap-tap.herokuapp.com",
+  route: {
+    login: ["GET", "/user"],
+    signup: ["POST", "/user"],
+    reset: ["POST", "/reset"],
+    push: ["POST", "/push"],
+    save: ["POST", "/save"],
+    quit: ["POST", "/quit"]
+  }
+};
 
-export const colors = []
+export const colors = [];
