5652e58c941e8c8f283dccad9555855eb1773e2e
justin-wolfe-z <justin.wolfe@zapier.com>
2018-12-15 10:46:32 -0500

    deleted unused hot reload code

--- src/index.js
+++ src/index.js
@@ -1,36 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { render } from 'react-dom'
-import { applyMiddleware, createStore } from 'redux'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
 //import { AppContainer } from 'react-hot-loader';
-import logger from 'redux-logger'
-import thunk from 'redux-thunk';
-import App from './components/App'
-import reducer from './reducers'
-import { queryThunk } from './actions/index'
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import App from "./components/App";
+import reducer from "./reducers";
+import { queryThunk } from "./actions/index";
 
-const store = createStore(reducer, applyMiddleware(thunk,logger))
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 render(
-  	<Provider store={store}>
-    	<App />
-    </Provider>,
-  document.getElementById('root')
-)
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
-store.dispatch(queryThunk(window.location.search))
-
-/*if (module.hot) {
-    module.hot.accept('./components/App', () => {
-        const NextApp = require('./components/App').default; 
-        ReactDOM.render(
-            <AppContainer>
-              	<Provider store={store}>
-                	<NextApp />
-                </Provider>
-            </AppContainer>,
-            document.getElementById('root')
-        );
-    });
-}*/
+store.dispatch(queryThunk(window.location.search));
