4fe3b9f9a96b69fc5436f29c9c27446bf59df47e
justin-wolfe-z <justin.wolfe@zapier.com>
2018-12-15 10:46:56 -0500

    deleted unused imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { render } from "react-dom";
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
-//import { AppContainer } from 'react-hot-loader';
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import App from "./components/App";
